ad1ba6daeaac95bc23f51b09a3c220c5bb7e953c
Jagapradeep <jagapradeep1104@gmail.com>
2021-03-07 23:04:19 +0530

    Deleted unwanted file

--- GetIt/App.js
+++ GetIt/App.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import MessagesScreen from "./App/Screens/MessagesScreen";
 import ListingEditScreen from "./App/Screens/ListingEditScreen";
-import Dummy from "./App/Screens/Dummy";
 
 export default function App() {
   return <MessagesScreen />;
